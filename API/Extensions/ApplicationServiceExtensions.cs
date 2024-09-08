5ed2de5a58c81a7f96283ecb39876762be70ed7e
daineliyas <daineliyas@gmail.com>
2024-09-08 16:47:47 -0600

    End of Section 4

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,22 @@
-﻿namespace API;
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
 
-public class ApplicationServiceExtensions
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
 {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddControllers();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+        services.AddScoped<ITokenService, TokenService>();
 
+        return services;
+    }
 }
