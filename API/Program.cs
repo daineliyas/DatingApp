7b5ee04164feefea1abdcdb7008817f44143238a
daineliyas <daineliyas@gmail.com>
2024-09-10 21:03:05 -0600

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
 
